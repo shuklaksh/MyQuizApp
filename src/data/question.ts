beccf3fbd957ba17f38ff9cb5105fc6145dcc61c
shuklaksh <shuklaksh156@gmail.com>
2024-11-04 23:32:08 +0530

    fix: image url

--- src/data/question.ts
+++ src/data/question.ts
@@ -19,28 +19,28 @@ const questions: Question[] = [
       question: "Select the prime numbers.",
       options: ["2", "4", "5", "9"],
       correctAnswers: ["2", "5"],
-      image: "https://thirdspacelearning.com/wp-content/uploads/2019/11/How-do-prime-numbers-work.png",
+      image: null,
   },
   {
       id: 3,
       question: "What is the largest mammal in the world?",
       options: ["Blue Whale", "African Elephant", "Great White Shark", "Giraffe"],
       correctAnswers: ["Blue Whale"],
-      image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Blue_Whale_%28Balaenoptera_musculus%29_in_the_Southern_Ocean.jpg",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjBjM0CasJNiZg5qrSlddf7Ir69sqg51chXQ&s",
   },
   {
       id: 4,
       question: "Which planet is known as the Red Planet?",
       options: ["Earth", "Mars", "Venus", "Jupiter"],
       correctAnswers: ["Mars"],
-      image: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Mars_After_Dusk.jpg",
+      image: "https://starwalk.space/gallery/images/mars-the-ultimate-guide/1920x1080.jpg",
   },
   {
       id: 5,
       question: "Who wrote the play 'Romeo and Juliet'?",
       options: ["Charles Dickens", "Mark Twain", "William Shakespeare", "Jane Austen"],
       correctAnswers: ["William Shakespeare"],
-      image: "https://upload.wikimedia.org/wikipedia/commons/6/6c/Romeo_and_Juliet.jpg",
+      image: null,
   },
 ];
 
